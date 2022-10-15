c0e4920ca7b0bf70c8e430f7800496245d8844d7
AndreBFarias <andre.dsbf@gmail.com>
2022-10-15 09:00:00 -0300

    refactor: pipeline incremental com consolidacao automatica
    - Pipeline detecta data maxima dos CSVs e baixa apenas dias faltantes
    - Consolidados em CSV UTF-8 com separador ; e datas dd/mm/yyyy
    - ZIP LZMA gerado automaticamente ao final da consolidacao
    - Removidas variaveis DAYS_BACK e BACKFILL_DAYS
    - run.bat unificado com criacao automatica de estrutura de pastas
    - Renomeado spec e exe para stilingue_etl (generico)

--- run.bat
+++ run.bat
@@ -1,3 +1,34 @@
 @echo off
-call .venv\Scripts\activate
-python -m src.pipeline
+setlocal enabledelayedexpansion
+
+set "BASE_DIR=%~dp0"
+cd /d "%BASE_DIR%"
+
+if not exist "data\csv\visao_geral" mkdir "data\csv\visao_geral"
+if not exist "data\csv\sentimento_grupos" mkdir "data\csv\sentimento_grupos"
+if not exist "data\csv\sentimento_temas" mkdir "data\csv\sentimento_temas"
+if not exist "data\csv\linechart" mkdir "data\csv\linechart"
+if not exist "data\csv\publicacoes" mkdir "data\csv\publicacoes"
+if not exist "data\csv\ranking_evolutivo" mkdir "data\csv\ranking_evolutivo"
+if not exist "consolidado" mkdir "consolidado"
+if not exist "logs" mkdir "logs"
+
+if not exist ".env" (
+    if exist ".env.example" (
+        copy /Y ".env.example" ".env" >nul
+        echo Arquivo .env criado. Edite com seu token antes de continuar.
+        notepad ".env"
+        pause
+        exit /b 0
+    ) else (
+        echo ERRO: .env.example nao encontrado.
+        pause
+        exit /b 1
+    )
+)
+
+if exist "stilingue_etl.exe" (
+    stilingue_etl.exe
+) else (
+    python -m src
+)
