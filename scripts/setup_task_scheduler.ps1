5e3d0938daf70bd44fd542d6a48f068c43ddd7ef
AndreBFarias <andre.dsbf@gmail.com>
2023-03-05 11:00:00 -0300

    fix: remover referência a cliente (Energisa) e atualizar .env.example
    - Renomear task scheduler de StilinguePipelineEnergisa para SocialListeningPipeline
    - Atualizar variáveis STILINGUE_* para SOCIAL_LISTENING_* no .env.example

--- scripts/setup_task_scheduler.ps1
+++ scripts/setup_task_scheduler.ps1
@@ -1,8 +1,8 @@
 param(
-    [string]$InstallDir = "C:\stilingue_etl"
+    [string]$InstallDir = "C:\whisper_pulse"
 )
 
 $Action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c $InstallDir\run.bat >> $InstallDir\logs\task.log 2>&1"
 $Trigger = New-ScheduledTaskTrigger -Daily -At "06:00AM"
 $Settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Hours 1)
-Register-ScheduledTask -TaskName "StilinguePipelineEnergisa" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest
+Register-ScheduledTask -TaskName "SocialListeningPipeline" -Action $Action -Trigger $Trigger -Settings $Settings -RunLevel Highest
